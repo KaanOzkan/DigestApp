a284e9415ff9782fa35ff58184335ade76d27bbe
Kaan Ozkan <kaanozkan97@gmail.com>
2018-02-03 16:17:53 -0500

    Initial Commit

---
+++ Digest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Digest
+//
+//  Created by Kaan Ozkan on 2018-02-03.
+//  Copyright © 2018 Kaan Ozkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
